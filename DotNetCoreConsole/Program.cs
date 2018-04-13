c984063c0c3a03a6e45c897883ed3b9afc2dcd70
Israel Ogbole <iogbole@yahoo.com>
2018-04-13 23:38:06 +0100

    Initial commit

---
+++ DotNetCoreConsole/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DotNetCoreConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var names = new System.Collections.Generic.List<string> { "Ojo", "Ana", "Chelly" };
+            Console.WriteLine();
+            names.Add("Luke");
+            names.Add("Ehi");
+            names.Remove("Ana");
+            foreach (var name in names)
+            {
+                Console.WriteLine($"Hello {name.ToUpper()}!");
+            }
+            Console.WriteLine("AppDynamics .Net Core Windows Instrumentation test");
+        }
+    }
+}
